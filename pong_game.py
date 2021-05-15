1c2d4a4c33a968f5599589c127bf8551bbb479d3
Levi alfaia loureiro <lal.snf20@uea.edu.br>
2021-05-15 00:30:12 -0300

    Fixing pep-8

--- pong_game.py
+++ pong_game.py
@@ -3,104 +3,99 @@ import turtle
 import winsound
 
 
-speed = 5
-count_s = 1
-colors = ["grey", "light blue", "blue", "dark blue", "light green", "green", "dark green", "yellow", "orange", "red"]
+def draw_screen():
+    screen.title("My Pong")
+    screen.bgcolor("black")
+    screen.setup(width=800, height=600)
+    screen.tracer(0)
 
-# Draw screen
-screen = turtle.Screen()
-screen.title("My Pong")
-screen.bgcolor("black")
-screen.setup(width=800, height=600)
-screen.tracer(0)
 
-# Draw paddle 1
-paddle_1 = turtle.Turtle()
-paddle_1.speed(0)
-paddle_1.shape("square")
-paddle_1.color("white")
-paddle_1.shapesize(stretch_wid=5, stretch_len=1)
-paddle_1.penup()
-paddle_1.goto(-350, 0)
+def draw_paddle_1():
+    paddle_1.speed(0)
+    paddle_1.shape("square")
+    paddle_1.color("white")
+    paddle_1.shapesize(stretch_wid=5, stretch_len=1)
+    paddle_1.penup()
+    paddle_1.goto(-350, 0)
 
-# Draw paddle 2
-paddle_2 = turtle.Turtle()
-paddle_2.speed(0)
-paddle_2.shape("square")
-paddle_2.color("white")
-paddle_2.shapesize(stretch_wid=5, stretch_len=1)
-paddle_2.penup()
-paddle_2.goto(350, 0)
 
-# Draw ball
-ball = turtle.Turtle()
-ball.speed(0)
-ball.shape("square")
-ball.color("white")
-ball.penup()
-ball.goto(0, 0)
+def draw_paddle_2():
+    paddle_2.speed(0)
+    paddle_2.shape("square")
+    paddle_2.color("white")
+    paddle_2.shapesize(stretch_wid=5, stretch_len=1)
+    paddle_2.penup()
+    paddle_2.goto(350, 0)
+
+
+def draw_ball():
+    ball.speed(0)
+    ball.shape("square")
+    ball.color("grey")
+    ball.penup()
+    ball.goto(0, 0)
+
 
 # Setting the ball speed
-ball.dx = speed
-ball.dy = speed
-
-# Score
-score_1 = 0
-score_2 = 0
-
-# Head-up display
-hud = turtle.Turtle()
-hud.speed(0)
-hud.shape("square")
-hud.color("white")
-hud.penup()
-hud.hideturtle()
-hud.goto(0, 0)
-hud.write("PRESS k TO START", align="center", font=("Press Start 2P", 24, "normal"))
+def ball_speed():
+    ball.dx = speed
+    ball.dy = speed
 
 
-def paddle_1_up():
-    y = paddle_1.ycor()
+# Move up
+def paddle_up(paddle):
+    y = paddle.ycor()
     if y < 250:
         y += 30
     else:
         y = 250
-    paddle_1.sety(y)
+    paddle.sety(y)
+
+
+# Move down
+def paddle_down(paddle):
+    y = paddle.ycor()
+    if y > -250:
+        y += -30
+    else:
+        y = -250
+    paddle.sety(y)
+
+
+# Paddles
+def paddle_1_up():
+    paddle_up(paddle_1)
 
 
 def paddle_1_down():
-    y = paddle_1.ycor()
-    if y > -250:
-        y += -30
-    else:
-        y = -250
-    paddle_1.sety(y)
+    paddle_down(paddle_1)
 
 
 def paddle_2_up():
-    y = paddle_2.ycor()
-    if y < 250:
-        y += 30
-    else:
-        y = 250
-    paddle_2.sety(y)
+    paddle_up(paddle_2)
 
 
 def paddle_2_down():
-    y = paddle_2.ycor()
-    if y > -250:
-        y += -30
-    else:
-        y = -250
-    paddle_2.sety(y)
+    paddle_down(paddle_2)
 
 
-# Keyboard
-screen.listen()
-screen.onkeypress(paddle_1_up, "w")
-screen.onkeypress(paddle_1_down, "s")
-screen.onkeypress(paddle_2_up, "Up")
-screen.onkeypress(paddle_2_down, "Down")
+def score_update(score):
+    score += 1
+
+
+def screen_game():
+    global count_s
+    count_s += 1
+    ball.color(colors[count_s - 1])
+    hud.clear()
+    hud.goto(0, 255)
+    hud.write("P1 {} : {} P2".format(score_1, score_2), align="center",
+              font=("Press Start 2P", 24, "normal"))
+    hud.goto(0, 225)
+    hud.write("SPEED x%d" % count_s, align="center", font=("Press Start 2P", 16, "normal"))
+    hud.goto(0, -270)
+    hud.write("PRESS k TO RESTART OR y TO FINISH", align="center",
+              font=("Press Start 2P", 12, "normal"))
 
 
 def victory():
@@ -129,6 +124,46 @@ def victory():
     turtle.done()
 
 
+speed = 5
+count_s = 1
+colors = ["grey", "light blue", "blue", "dark blue", "light green", "green", "dark green", "yellow", "orange", "red"]
+
+# Drawing the game
+screen = turtle.Screen()
+paddle_1 = turtle.Turtle()
+paddle_2 = turtle.Turtle()
+ball = turtle.Turtle()
+
+draw_screen()
+draw_paddle_1()
+draw_paddle_2()
+draw_ball()
+
+ball_speed()
+
+# Score
+score_1 = 0
+score_2 = 0
+
+# Head-up display
+hud = turtle.Turtle()
+hud.speed(0)
+hud.shape("square")
+hud.color("white")
+hud.penup()
+hud.hideturtle()
+hud.goto(0, 0)
+hud.write("PRESS k TO START", align="center", font=("Press Start 2P", 24, "normal"))
+
+
+# Keyboard
+screen.listen()
+screen.onkeypress(paddle_1_up, "w")
+screen.onkeypress(paddle_1_down, "s")
+screen.onkeypress(paddle_2_up, "Up")
+screen.onkeypress(paddle_2_down, "Down")
+
+
 def screen_update():
     global score_1
     global score_2
@@ -137,6 +172,7 @@ def screen_update():
     score_1 = 0
     score_2 = 0
     count_s = 1
+    ball.color(colors[count_s - 1])
 
     ball.goto(0, 0)
     paddle_1.goto(-350, 0)
@@ -157,7 +193,7 @@ def screen_update():
         screen.update()
 
         # Setting ball acceleration
-        time.sleep(1/60)
+        time.sleep(1/70)
 
         # Ball movement
         ball.setx(ball.xcor() + ball.dx)
@@ -177,51 +213,25 @@ def screen_update():
             ball.sety(-290)
             ball.dy *= -1
 
-        # Collision with left wall
-        if ball.xcor() < -390:
-            score_2 += 1
-            count_s = 1
+        # Collision with the side walls
+        if ball.xcor() > 390 or ball.xcor() < -390:
+            flag = 1
+            count_s = 0
+            if ball.xcor() > 390:
+                score_1 += 1
+                flag = 1
+            elif ball.xcor() < -390:
+                score_2 += 1
+                flag = -1
 
-            ball.color(colors[count_s - 1])
-
-            hud.clear()
-            hud.goto(0, 255)
-            hud.write("P1 {} : {} P2".format(score_1, score_2), align="center", font=("Press Start 2P", 24, "normal"))
-            hud.goto(0, 225)
-            hud.write("SPEED x%d" % count_s, align="center", font=("Press Start 2P", 16, "normal"))
-            hud.goto(0, -270)
-            hud.write("PRESS k TO RESTART OR y TO FINISH", align="center", font=("Press Start 2P", 12, "normal"))
+            screen_game()
 
             # Setting the sound to Windows
             winsound.PlaySound("258020__kodack__arcade-bleep-sound.wav", winsound.SND_ASYNC)
 
             ball.goto(0, 0)
-            ball.dx = speed
+            ball.dx = speed * flag
             ball.dy = speed
-            ball.dx *= -1
-
-        # Collision with right wall
-        if ball.xcor() > 390:
-            score_1 += 1
-            count_s = 1
-
-            ball.color(colors[count_s - 1])
-
-            hud.clear()
-            hud.goto(0, 255)
-            hud.write("P1 {} : {} P2".format(score_1, score_2), align="center", font=("Press Start 2P", 24, "normal"))
-            hud.goto(0, 225)
-            hud.write("SPEED x%d" % count_s, align="center", font=("Press Start 2P", 16, "normal"))
-            hud.goto(0, -270)
-            hud.write("PRESS k TO RESTART OR y TO FINISH", align="center", font=("Press Start 2P", 12, "normal"))
-
-            # Setting the sound to Windows
-            winsound.PlaySound("258020__kodack__arcade-bleep-sound.wav", winsound.SND_ASYNC)
-
-            ball.goto(0, 0)
-            ball.dx = speed
-            ball.dy = speed
-            ball.dx *= 1
 
         # Fixing the bug
         # Collision with the paddle 1
@@ -233,22 +243,10 @@ def screen_update():
                 ball.dx *= -1
 
                 if ball.dx < 9.5:
-                    count_s += 1
 
-                    ball.color(colors[count_s - 1])
-
-                    hud.clear()
-                    hud.goto(0, 255)
-                    hud.write("P1 {} : {} P2".format(score_1, score_2), align="center",
-                              font=("Press Start 2P", 24, "normal"))
-                    hud.goto(0, 225)
-                    hud.write("SPEED x%d" % count_s, align="center", font=("Press Start 2P", 16, "normal"))
-                    hud.goto(0, -270)
-                    hud.write("PRESS k TO RESTART OR y TO FINISH", align="center",
-                              font=("Press Start 2P", 12, "normal"))
+                    screen_game()
 
                     ball.dx = (ball.dx + 0.5)
-                    print(ball.dx)
 
                 # Setting the sound to Windows
                 winsound.PlaySound("bounce.wav", winsound.SND_ASYNC)
@@ -267,22 +265,9 @@ def screen_update():
                 ball.dx *= -1
 
                 if ball.dx > -9.5:
-                    count_s += 1
-
-                    ball.color(colors[count_s - 1])
-
-                    hud.clear()
-                    hud.goto(0, 255)
-                    hud.write("P1 {} : {} P2".format(score_1, score_2), align="center",
-                              font=("Press Start 2P", 24, "normal"))
-                    hud.goto(0, 225)
-                    hud.write("SPEED x%d" % count_s, align="center", font=("Press Start 2P", 16, "normal"))
-                    hud.goto(0, -270)
-                    hud.write("PRESS k TO RESTART OR y TO FINISH", align="center",
-                              font=("Press Start 2P", 12, "normal"))
+                    screen_game()
 
                     ball.dx = (ball.dx - 0.5)
-                    print(ball.dx)
 
                 # Setting the sound to Windows
                 winsound.PlaySound("bounce.wav", winsound.SND_ASYNC)
